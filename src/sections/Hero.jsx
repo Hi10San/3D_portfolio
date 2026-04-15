6a885d83e327c0ee5adbddb2295177e3060ec7b6
HitenSan <prajapati.hiten05@gmail.com>
2026-04-16 01:09:44 +0530

    fix spline

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,15 +4,15 @@ import Loader from "../components/Loader";
 
 const Hero = () => {
   return (
-    <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden">
-      <figure
-        className="absolute inset-0"
-        style={{ width: "100vw", height: "100vh" }}
-      >
+    <section className="min-h-screen overflow-hidden">
+      
+      {/* isolate Spline */}
+      <div style={{ width: "100vw", height: "100vh" }}>
         <Suspense fallback={<Loader />}>
           <Spline scene="https://prod.spline.design/4qkyCaJdjrewNVBZ/scene.splinecode" />
         </Suspense>
-      </figure>
+      </div>
+
     </section>
   );
 };

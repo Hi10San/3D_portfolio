b5f2f521117f098a71dc8b2fa48bc95ac238dceb
HitenSan <prajapati.hiten05@gmail.com>
2026-04-16 18:21:04 +0530

    code is crafted section fixed

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -35,19 +35,19 @@ const About = () => {
             <div className="grid-default-color grid-2">
                 <div ref = {grid2Container} className="flex items-center justify-center w-full h-full">
                     <p className="flex items-end text 5xl text-gray-500">CODE IS CRAFT</p>
-                    <Card style = {{ rotate: "75deg" , top: "30%", left: "20%"}} text="GRASP"
+                    <Card style = {{ rotate: "75deg" , top: "30%", left: "20%"}} text="JAVA"
                     containerRef = {grid2Container}
                     />
-                    <Card style = {{ rotate: "-30deg" , top: "60%", left: "45%"}} text="hfda"
+                    <Card style = {{ rotate: "-30deg" , top: "60%", left: "45%"}} text="React"
                     containerRef = {grid2Container}
                     />
-                    <Card style = {{ rotate: "-45deg" , top: "55%", left: "20%"}} text="tthd"
+                    <Card style = {{ rotate: "-45deg" , top: "55%", left: "20%"}} text="JavaScript"
                     containerRef = {grid2Container}
                     />
-                    <Card style = {{ rotate: "-60deg" , top: "50%", left: "70%"}} text="letsgoo"
+                    <Card style = {{ rotate: "-60deg" , top: "50%", left: "70%"}} text="TailWind"
                     containerRef = {grid2Container}
                     />
-                    <Card style = {{ rotate: "-20deg" , top: "80%", left: "10%"}} text="WHA' DA HELL"
+                    <Card style = {{ rotate: "-20deg" , top: "80%", left: "10%"}} text="Node>js"
                     containerRef = {grid2Container}
                     />
                     <Card style = {{ rotate: "30deg" , top: "90%", left: "50%"}} image = "assets/logos/csharp-pink.png"

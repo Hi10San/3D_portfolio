b2c41f2dc740eda93a5ee281d1384b52913d5ee3
HitenSan <prajapati.hiten05@gmail.com>
2026-04-17 17:51:40 +0530

    fix the centre issue

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,29 +1,15 @@
-import Spline from "@splinetool/react-spline";
+import Spline from '@splinetool/react-spline';
+import { Suspense } from "react";
 import Loader from "../components/Loader";
-import { Suspense, useEffect, useState } from "react";
 
 const Hero = () => {
-  const [loadSpline, setLoadSpline] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoadSpline(true);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
-    <section className="flex items-start justify-center min-h-screen overflow-hidden">
-      <figure className="absolute inset-0 w-screen h-screen">
-        {loadSpline ? (
-          <Suspense fallback={<Loader />}>
-            <Spline scene="https://prod.spline.design/4qkyCaJdjrewNVBZ/scene.splinecode" />
-          </Suspense>
-        ) : (
-          <Loader />
-        )}
-      </figure>
+    <section className="min-h-screen min-w-screen overflow-hidden">
+      <Suspense fallback={<Loader />}>
+        <Spline scene="https://prod.spline.design/4qkyCaJdjrewNVBZ/scene.splinecode" 
+          style={{ width: "100vw", height: "100vh" }}
+        />
+      </Suspense>
     </section>
   );
 };
